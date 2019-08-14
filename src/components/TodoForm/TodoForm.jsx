0354578963ea2de16746264f03a6b96699006649
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-14 10:22:22 -0400

    baisc formik setup

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
-const TodoForm = () => {
-  return (<></>);
+const TodoForm = ({ values, errors, touched, status }) => {
+  useEffect(() => {
+    console.log(status);
+  }, [status]);
+
+  return (<>
+    <Form>
+      {errors.task && touched.task && <p>{errors.task}</p>}
+      <Field type="text" name="task" placeholder="Task" />
+      <button type="submit">Add Taks</button>
+      <button onClick={() => {}}>Clear Completed</button>
+    </Form>
+  </>);
 };
 
-export default TodoForm;
+export default withFormik({
+  mapPropsToValues({ task }) {
+    return {
+      task: task || '',
+    }
+  },
+
+  validationSchema: Yup.object().shape({
+    task: Yup.string()
+      .required('Enter a task'),
+  }),
+
+  handleSubmit(values, { setStatus, resetForm }) {
+    setStatus(values);
+    resetForm();
+  }
+})(TodoForm);
